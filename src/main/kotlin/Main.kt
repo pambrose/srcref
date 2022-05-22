9efd9aafa8f43d0a5c200e0651cdae65fb5c18bb
Paul Ambrose <pambrose@mac.com>
2022-05-22 14:51:21 -0700

    Work around heroku error

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 import QueryArgs.ACCOUNT
 import QueryArgs.BRANCH
 import QueryArgs.PATH
-import QueryArgs.REGEX
 import QueryArgs.REPO
 import SrcRef.githubRefUrl
 import SrcRef.githubref
@@ -132,10 +131,10 @@ fun main() {
                     }
                     tr {
                       td { +"Match Expr:" }
-                      td {
-                        val pv = params[REGEX.arg] ?: ""
-                        textInput { name = REGEX.arg; size = "20"; required = true; value = pv }
-                      }
+//                      td {
+//                        val pv = params[REGEX.arg] ?: ""
+//                        textInput { name = REGEX.arg; size = "20"; required = true; value = pv }
+//                      }
                     }
 //                    tr {
 //                      td {  +"Offset:"  }
