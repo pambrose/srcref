a4736b1bf118855bb2eccbadfbed2ace161b1e9c
Paul Ambrose <pambrose@mac.com>
2022-05-22 13:21:02 -0700

    Convert url values into text areas

--- src/main/kotlin/SrcRef.kt
+++ src/main/kotlin/SrcRef.kt
@@ -7,8 +7,10 @@ import QueryArgs.REGEX
 import QueryArgs.REPO
 import QueryArgs.TOPDOWN
 import SrcRef.githubRefUrl
+import SrcRef.githubref
 import SrcRef.logger
 import SrcRef.queryParams
+import SrcRef.urlPrefix
 import com.github.pambrose.common.response.*
 import com.github.pambrose.common.util.*
 import io.ktor.server.application.*
@@ -24,18 +26,7 @@ import kotlinx.html.dom.*
 import mu.*
 import java.net.*
 
-enum class QueryArgs {
-  ACCOUNT, REPO, BRANCH, PATH, REGEX, OFFSET, OCCURENCE, TOPDOWN;
-
-  val arg get() = name.lowercase()
-}
-
 fun main() {
-  fun HTMLTag.rawHtml(html: String) = unsafe { raw(html) }
-
-  val prefix = (System.getenv("PREFIX") ?: "http://localhost:8080").removeSuffix("/")
-  val githubref = "githubRef"
-
   embeddedServer(CIO, port = System.getenv("PORT")?.toInt() ?: 8080) {
     install(CallLogging)
     install(DefaultHeaders) { header("X-Engine", "Ktor") }
@@ -59,8 +50,7 @@ fun main() {
                 }
                 script {
                   rawHtml("\n")
-                  unsafe {
-                    """
+                  +"""
                     function copyToClipboard(textToCopy) {
                         // navigator clipboard api needs a secure context (https)
                         if (navigator.clipboard && window.isSecureContext) {
@@ -92,7 +82,6 @@ fun main() {
                       //.then(() => alert("Copied the text: " + copyText.value));
                     }
                   """.trimIndent().prependIndent("\t\t")
-                  }
                   rawHtml("\n\t\t")
                 }
 
@@ -127,27 +116,38 @@ fun main() {
                   table {
                     tr {
                       td { style = ""; label { +"Org Name/Username:" } }
-                      td { textInput { name = ACCOUNT.arg; size = "20"; value = params[ACCOUNT.arg] ?: "" } }
+                      td {
+                        textInput {
+                          name = ACCOUNT.arg; size = "20"; required = true; value = params[ACCOUNT.arg] ?: ""
+                        }
+                      }
                     }
                     tr {
                       td { style = ""; label { +"Repo Name:" } }
-                      td { textInput { name = REPO.arg; size = "20"; value = params[REPO.arg] ?: "" } }
+                      td { textInput { name = REPO.arg; size = "20"; required = true; value = params[REPO.arg] ?: "" } }
                     }
                     tr {
                       td { style = ""; label { +"Branch Name:" } }
-                      td { textInput { name = BRANCH.arg; size = "20"; value = params[BRANCH.arg] ?: "master" } }
+                      val p = (params[BRANCH.arg] ?: "").let { if (it.isBlank()) "master" else it }
+                      td { textInput { name = BRANCH.arg; size = "20"; required = true; value = p } }
                     }
                     tr {
                       td { style = ""; label { +"File Path:" } }
-                      td { textInput { name = PATH.arg; size = "70"; value = params[PATH.arg] ?: "/src/main/kotlin/" } }
+                      val p = (params[PATH.arg] ?: "").let { if (it.isBlank()) "/src/main/kotlin/" else it }
+                      td { textInput { name = PATH.arg; size = "70"; required = true; value = p } }
                     }
                     tr {
                       td { style = ""; label { +"Match Expr:" } }
-                      td { textInput { name = REGEX.arg; size = "20"; value = params[REGEX.arg] ?: "" } }
+                      td {
+                        textInput {
+                          name = REGEX.arg; size = "20"; required = true; value = params[REGEX.arg] ?: ""
+                        }
+                      }
                     }
                     tr {
                       td { style = ""; label { +"Offset:" } }
-                      td { textInput { name = OFFSET.arg; size = "10"; value = params[OFFSET.arg] ?: "0" } }
+                      val p = (params[OFFSET.arg] ?: "").let { if (it.isBlank()) "0" else it }
+                      td { textInput { name = OFFSET.arg; size = "10"; required = true; value = p } }
                     }
                     tr {
                       td { style = ""; label { +"Occurence:" } }
@@ -155,7 +155,8 @@ fun main() {
                         select {
                           name = OCCURENCE.arg
                           size = "1"
-                          val isSelected = (params[OCCURENCE.arg] ?: "1").toInt()
+                          val p = (params[OCCURENCE.arg] ?: "").let { if (it.isBlank()) "1" else it }
+                          val isSelected = p.toInt()
                           option { +" 1st "; value = "1"; selected = isSelected == 1 }
                           option { +" 2nd "; value = "2"; selected = isSelected == 2 }
                           option { +" 3rd "; value = "3"; selected = isSelected == 3 }
@@ -173,7 +174,8 @@ fun main() {
                       td { style = ""; label { +"Search Direction:" } }
                       td {
                         span {
-                          val isChecked = (params[TOPDOWN.arg] ?: "true").toBoolean()
+                          val p = (params[TOPDOWN.arg] ?: "").let { if (it.isBlank()) "true" else it }
+                          val isChecked = p.toBoolean()
                           style = "text-align:center"
                           radioInput { id = "topdown"; name = TOPDOWN.arg; value = "true"; checked = isChecked }
                           label {
@@ -199,15 +201,15 @@ fun main() {
 
                 if (params.values.asSequence().filter { it.isNotBlank() }.any()) {
                   val args = params.map { (k, v) -> "$k=${v.encode()}" }.joinToString("&")
-                  val url = "$prefix/$githubref?$args"
+                  val url = "$urlPrefix/$githubref?$args"
                   val ghurl = githubRefUrl(params)
 
                   div {
                     style = "padding-left: 25px;"
                     br {}
-                    input { id = "urlval"; type = InputType.text; value = url; size = "125px" }
+                    textArea { id = "urlval"; rows = "3";+url; cols = "91"; readonly = true }
                     p { +"will redirect to:" }
-                    input { id = "ghurlval"; type = InputType.text; value = ghurl; size = "125px" }
+                    textArea { id = "ghurlval"; rows = "1"; +ghurl; cols = "91"; readonly = true }
                     p {}
                     button { onClick = "copyUrl()"; +"Copy URL" }
                     span { +" " }
@@ -231,7 +233,17 @@ fun main() {
   }.start(wait = true)
 }
 
+fun HTMLTag.rawHtml(html: String) = unsafe { raw(html) }
+
+enum class QueryArgs {
+  ACCOUNT, REPO, BRANCH, PATH, REGEX, OFFSET, OCCURENCE, TOPDOWN;
+
+  val arg get() = name.lowercase()
+}
+
 object SrcRef : KLogging() {
+  const val githubref = "githubRef"
+  val urlPrefix = (System.getenv("PREFIX") ?: "http://localhost:8080").removeSuffix("/")
 
   val PipelineContext<Unit, ApplicationCall>.queryParams
     get() =
@@ -245,23 +257,26 @@ object SrcRef : KLogging() {
             }
         }
 
-  fun githubRefUrl(params: Map<String, String>): String {
-    val account = params[ACCOUNT.arg] ?: ""
-    val repo = params[REPO.arg] ?: ""
-    val path = params[PATH.arg] ?: ""
-    val branch = params[BRANCH.arg] ?: ""
-    val url = githubRawUrl(account, repo, path, branch)
-    val lines = URL(url).readText().lines()
-    val linenum =
-      calcLineNumber(
-        lines,
-        params[TOPDOWN.arg]?.toBoolean() ?: true,
-        params[REGEX.arg] ?: "",
-        params[OCCURENCE.arg]?.toInt() ?: 1,
-        params[OFFSET.arg]?.toInt() ?: 0
-      )
-    return githubSourceUrl(account, repo, path, branch, linenum)
-  }
+  fun githubRefUrl(params: Map<String, String>) =
+    try {
+      val account = params[ACCOUNT.arg] ?: throw IllegalArgumentException()
+      val repo = params[REPO.arg] ?: throw IllegalArgumentException()
+      val path = params[PATH.arg] ?: throw IllegalArgumentException()
+      val branch = params[BRANCH.arg] ?: throw IllegalArgumentException()
+      val url = githubRawUrl(account, repo, path, branch)
+      val lines = URL(url).readText().lines()
+      val linenum =
+        calcLineNumber(
+          lines,
+          params[TOPDOWN.arg]?.toBoolean() ?: true,
+          params[REGEX.arg] ?: "",
+          params[OCCURENCE.arg]?.toInt() ?: 1,
+          params[OFFSET.arg]?.toInt() ?: 0
+        )
+      githubSourceUrl(account, repo, path, branch, linenum)
+    } catch (e: Throwable) {
+      "Invalid"
+    }
 
   fun calcLineNumber(lines: List<String>, topDown: Boolean, pattern: String, occurence: Int, offset: Int) =
     try {
