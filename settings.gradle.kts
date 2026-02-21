d3e729bdcea858345e3c6fc0995664d705fd90d0
pambrose <pambrose@mac.com>
2026-02-20 22:19:55 -0800

    Update gradle-plugins version to 1.0.4; adjust root project name in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,10 @@ plugins {
   id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
 
-rootProject.name = "srcref"
-
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
   }
 }
 
-
+rootProject.name = "srcref"
